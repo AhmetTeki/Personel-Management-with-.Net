d83eb5afca8220503b815153febf18b8c02645a0
Ahmet Tekin <ahmettekin023402@gmail.com>
2025-04-24 23:04:18 +0300

    12.dakikada kaldım

--- PersonelManagement.UI/Controllers/AccountController.cs
+++ PersonelManagement.UI/Controllers/AccountController.cs
@@ -1,8 +1,13 @@
 ﻿
+using Humanizer;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 using PersonelManagement.Application.Requests;
+using System.Security.Claims;
+using PersonelManagement.Application.Dtos;
 
 namespace PersonelManagement.UI.Controllers
 {
@@ -23,7 +28,26 @@ namespace PersonelManagement.UI.Controllers
         public async Task<IActionResult> Login(LoginRequest request)
         {
            var result=await this._mediator.Send(request);
-            return View();
+            if (result.IsSucces)
+            {
+                return RedirectToAction("", "", new {area=""});
+            }
+            else
+            {
+                if (result.Errors != null && result.Errors.Count > 0)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", result.ErrorMassage ?? "Bilinmeyen bir hata oluştu, sistem üreticinize başvurun.");
+                }
+                return View(request);
+            }
+            
         }
         public IActionResult Register()
         {
@@ -32,6 +56,49 @@ namespace PersonelManagement.UI.Controllers
         public IActionResult LogOut()
         {
             return View();
+        }
+        private async Task SetAuthCookie(LoginResponseDto dto)
+        {
+           // User.Claims.SingleOrDefault(x => x.Type == ClaimTypes.Name);
+            var claims = new List<Claim>
+            {
+                new Claim("UserId",dto.Id+""),
+            new Claim("Name", dto.Name),
+            new Claim("Surname", dto.Surname),
+            new Claim(ClaimTypes.Role, dto.Role.ToString()),
+        };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                //AllowRefresh = <bool>,
+                // Refreshing the authentication session should be allowed.
+
+                ExpiresUtc = DateTimeOffset.UtcNow.AddDays(30),
+                // The time at which the authentication ticket expires. A 
+                // value set here overrides the ExpireTimeSpan option of 
+                // CookieAuthenticationOptions set with AddCookie.
+
+                IsPersistent = rememberMe,
+                // Whether the authentication session is persisted across 
+                // multiple requests. When used with cookies, controls
+                // whether the cookie's lifetime is absolute (matching the
+                // lifetime of the authentication ticket) or session-based.
+
+                //IssuedUtc = <DateTimeOffset>,
+                // The time at which the authentication ticket was issued.
+
+                //RedirectUri = <string>
+                // The full path or absolute URI to be used as an http 
+                // redirect response value.
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
         }
     }
 }
--- PersonelManagement.UI/Program.cs
+++ PersonelManagement.UI/Program.cs
@@ -1,8 +1,19 @@
 using PersonelManagement.Persistance;
 using PersonelManagement.Application.Extensions;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(opt =>
+    {
+        opt.Cookie.Name = "LoginCokiee";
+        opt.Cookie.HttpOnly = true;
+        opt.Cookie.SameSite = SameSiteMode.Strict;
+        opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    });
+
 
 builder.Services.AddPersistanceServices(builder.Configuration);
 
@@ -11,6 +22,10 @@ builder.Services.AddApplicationServices();
 var app = builder.Build();
 
 app.UseStaticFiles();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseRouting();
 
 
